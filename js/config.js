f7654e43ad7e9856f518f1236320cf905e6bb312
Ashera Silva <asherasilva100@gmail.com>
2019-03-23 09:29:56 +0530

    Add Event Completed, Login completed, Styling chnaged

--- js/config.js
+++ js/config.js
@@ -1,9 +1,83 @@
-var config = {
-    apiKey: "AIzaSyDx0RamPdbmvOaQVri2f17BctnRsg6X_zE",
-    authDomain: "memorymate-4233d.firebaseapp.com",
-    databaseURL: "https://memorymate-4233d.firebaseio.com",
-    projectId: "memorymate-4233d",
-    storageBucket: "memorymate-4233d.appspot.com",
-    messagingSenderId: "326325516705"
-};
-firebase.initializeApp(config);
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBElcSBIqe2koIyB6pD9aAFglAq82Fu90c",
+    authDomain: "hackaholics-memorymate.firebaseapp.com",
+    databaseURL: "https://hackaholics-memorymate.firebaseio.com",
+    projectId: "hackaholics-memorymate",
+    storageBucket: "hackaholics-memorymate.appspot.com",
+    messagingSenderId: "618931079152"
+  };
+  var app = firebase.initializeApp(config);
+  var db = firebase.firestore(app);
+
+// Function to register the user start
+var register = function() {
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+
+    if(ValidateEmail(email) & checkPasswordLength(password)) {
+
+
+        firebase.auth().createUserWithEmailAndPassword(email.value, password.value).then(function() {
+          alert("Registered Successfully");
+          window.location.href = './app.html';
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+        });
+
+    }
+}
+// Function to register the user end
+
+// Function to authenticate the user credentials start
+var login = function() {
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(function() {
+        
+        alert("Login Success.");
+        // redirect to app page
+        window.location.href = "./pages/app.html";
+
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("Login failed\n" + errorMessage);
+        // ...
+    });
+}
+// Function to authenticate the user credentials end
+
+
+
+function checkPasswordLength(inputtxt) {
+    var field = inputtxt.value;
+    if(field.length < 6)
+    {
+        alert("Password length is not enough.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function ValidateEmail(inputText) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!inputText.value.match(mailformat))
+    {
+        alert("You have entered an invalid email address!");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
--- js/dbFunction.js
+++ js/dbFunction.js
@@ -1,5 +1,16 @@
 // Function to retrieve data from the forms start
-var addEvent = function() {
+var config = {
+    apiKey: "AIzaSyBElcSBIqe2koIyB6pD9aAFglAq82Fu90c",
+    authDomain: "hackaholics-memorymate.firebaseapp.com",
+    databaseURL: "https://hackaholics-memorymate.firebaseio.com",
+    projectId: "hackaholics-memorymate",
+    storageBucket: "hackaholics-memorymate.appspot.com",
+    messagingSenderId: "618931079152"
+  };
+  var app = firebase.initializeApp(config);
+  var db = firebase.firestore(app);
+
+function addEvent() {
     var event_name = document.getElementById("event_name");
     var description = document.getElementById("description");
     var date = document.getElementById("date");
@@ -35,31 +46,51 @@ var addEvent = function() {
 
 // Function to pass data to the database start
 function addEventToDatabase(event_name, description, date, time, privacy, schedule) {
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            firebase.database().ref('events/' + user.uid + 'event/').set({
-                eventName: event_name,
-                description: description,
-                date: date,
-                time: time,
-                privacyType: privacy,
-                schedule: schedule
-            });
-        }
-        else {
-                alert("User Not Found");
-        }
-    });
+    db.collection("events").add({
+    name: event_name,
+    description: description,
+    date: date,
+    time: time,
+    privacy: privacy,
+    schedule: schedule
+  })
+  .then(function(docRef){
+    console.log("Success");
+    window.location.replace("./app.html");
+  })
+  .catch(function(err){
+    console.error("Failed!");
+  });
 }
 // Function to pass data to the database end
 
-function getEvents() {
-    alert("dsasd");
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            firebase.database().ref('events/' + user.uid).once('value', function(snapshot) {
-                console.log(snapshot.val());
-            });
-        }
+function getEventList(){
+    // db.collection("events").get().then(function(querySnapshot) {
+    //     querySnapshot.forEach(function(doc) {
+    //         // doc.data() is never undefined for query doc snapshots
+    //         console.log(doc.id, " => ", doc.data());
+    //     });
+    // });
+    var temp = 1;
+    db.collection("events").where("date", "==", "23-03-2019")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            var current_name = 'name'+temp
+            var current_description = 'desc'+temp
+            var current_time = 'time'+temp
+            temp+=1;
+
+            document.getElementById(current_name).innerHTML = doc.data().name;
+            document.getElementById(current_description).innerHTML = doc.data().description;
+            document.getElementById(current_time).innerHTML = doc.data().time;
+
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
     });
+
 }
